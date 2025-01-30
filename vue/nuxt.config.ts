172893fa68332022c45b45ae9813d73b84b7923a
iolite312 <68647023+iolite312@users.noreply.github.com>
2025-01-30 16:12:12 +0000

    adding eslint

--- vue/nuxt.config.ts
+++ vue/nuxt.config.ts
@@ -2,7 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@pinia/nuxt', '@vueuse/nuxt'],
+  modules: [
+    '@nuxt/ui', 
+    '@pinia/nuxt', 
+    '@vueuse/nuxt', 
+    '@nuxt/eslint'
+  ],
   css: ['~/assets/css/main.css'],
   ssr: false,
   icon: {
