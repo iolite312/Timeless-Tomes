2a1b43e3382bec2588735d63c2abfa6e17097459
iolite312 <68647023+iolite312@users.noreply.github.com>
2025-01-31 13:47:38 +0000

    .

--- vue/pages/index.vue
+++ vue/pages/index.vue
@@ -1,22 +1,10 @@
 <template>
-    <div>
-        <UButton label="Show toast" color="neutral" variant="outline" @click="showToast" />
-    </div>
+  <div>
+    <p>home</p>
+  </div>
 </template>
 
 <script setup lang="ts">
-const toast = useToast()
-
-function showToast() {
-  toast.add({
-    title: 'Uh oh! Something went wrong.',
-    description: 'There was a problem with your request.',
-    icon: 'i-lucide-wifi',
-    color: "error"
-  })
-}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
