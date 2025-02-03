1167ecf881824476730765e1805a90f7e139eccd
iolite312 <68647023+iolite312@users.noreply.github.com>
2025-02-03 16:01:20 +0000

    Error page

---
+++ vue/error.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <UApp>
+            <NuxtLayout>
+                <h1>{{ props.error.statusCode }}</h1>
+                <p>{{ props.error.message }}</p>
+                <NuxtLink to="/">Go back home</NuxtLink>
+            </NuxtLayout>
+        </UApp>
+    </div>
+</template>
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+const props = defineProps<{
+    error: NuxtError
+}>()
+</script>
