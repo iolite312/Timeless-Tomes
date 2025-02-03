f11802cae0ac5d1ff96cd1ec6b9d82decb64e45f
iolite312 <68647023+iolite312@users.noreply.github.com>
2025-02-03 16:01:08 +0000

    Creating a default layout

---
+++ vue/layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <HeaderComponent />
+        <div class="mx-12 flex items-center justify-center flex-col h-full">
+            <slot />
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
