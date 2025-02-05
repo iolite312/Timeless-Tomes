ef7ef25610626846b5b78c30c07d76a37b566934
iolite312 <68647023+iolite312@users.noreply.github.com>
2025-02-05 20:05:09 +0000

    Form validation on error from backend

--- vue/pages/login.vue
+++ vue/pages/login.vue
@@ -7,6 +7,7 @@
   >
     <template #login>
       <UForm
+        ref="loginForm"
         :schema="loginSchema"
         :state="state"
         class="flex flex-col gap-4"
@@ -30,16 +31,17 @@
 
     <template #register>
       <UForm
+        ref="registerForm"
         :schema="registerSchema"
         :state="state"
         class="flex flex-col gap-4"
         @submit="validateRegister"
       >
-        <UFormField label="First name" name="firstname">
-          <UInput v-model="state.firstname" class="w-full" />
+        <UFormField label="First name" name="first_name">
+          <UInput v-model="state.first_name" class="w-full" />
         </UFormField>
-        <UFormField label="Last name" name="lastname">
-          <UInput v-model="state.lastname" class="w-full" />
+        <UFormField label="Last name" name="last_name">
+          <UInput v-model="state.last_name" class="w-full" />
         </UFormField>
         <UFormField label="Email" name="email">
           <UInput v-model="state.email" class="w-full" />
@@ -68,8 +70,8 @@
 
 <script setup lang="ts">
 import * as z from 'zod';
-import type { FormSubmitEvent } from '@nuxt/ui';
-import type { Login } from '~/types';
+import type { FormSubmitEvent, FormError } from '@nuxt/ui';
+import type { Login, Register } from '~/types';
 
 definePageMeta({
   middleware: 'auth',
@@ -87,6 +89,8 @@ const items = [
     slot: 'register',
   },
 ];
+const registerForm = useTemplateRef('registerForm');
+const loginForm = useTemplateRef('loginForm');
 
 const loginSchema = z.object({
   email: z.string().email('Invalid email'),
@@ -95,8 +99,8 @@ const loginSchema = z.object({
 
 const registerSchema = z
   .object({
-    firstname: z.string().min(2, 'First name must be at least 2 characters'),
-    lastname: z.string().min(2, 'Last name must be at least 2 characters'),
+    first_name: z.string().min(2, 'First name must be at least 2 characters'),
+    last_name: z.string().min(2, 'Last name must be at least 2 characters'),
     email: z.string().email('Invalid email'),
     password: z.string().min(6, 'Password must be at least 6 characters'),
     confirmPassword: z.string(),
@@ -110,31 +114,66 @@ type LoginSchema = z.output<typeof loginSchema>;
 type RegisterSchema = z.output<typeof registerSchema>;
 
 const state = ref({
-  firstname: '',
-  lastname: '',
-  email: 'test@gmail.com',
-  password: '12345678',
+  first_name: '',
+  last_name: '',
+  email: '',
+  password: '',
   confirmPassword: '',
 });
 
 const toast = useToast();
 const userStore = useAccountStore();
+const formError: FormError[] = [];
+
 async function validateLogin(event: FormSubmitEvent<LoginSchema>) {
-  toast.add({
-    title: 'Success',
-    description: 'The form has been submitted.',
-    color: 'success',
-  });
-  userStore.login(event.data as Login);
-  console.log(userStore.account);
+  userStore
+    .login(event.data as Login)
+    .then(() => {
+      useRouter().push('/');
+    })
+    .catch(() => {
+      formError.push(
+        {
+          name: 'password',
+          message: 'Invalid email or password',
+        },
+        {
+          name: 'email',
+          message: 'Invalid email or password',
+        }
+      );
+      loginForm?.value?.setErrors(formError);
+      toast.add({
+        title: 'Error',
+        description: 'Something went wrong',
+        color: 'error',
+      });
+    });
 }
 
 async function validateRegister(event: FormSubmitEvent<RegisterSchema>) {
-  toast.add({
-    title: 'Success',
-    description: 'The form has been submitted.',
-    color: 'success',
-  });
-  console.log(event.data);
+  formError.length = 0;
+  userStore
+    .register(event.data as Register)
+    .then(() => {
+      useRouter().push('/');
+    })
+    .catch((error) => {
+      if (error.response.data.status === 422) {
+        console.log(error.response.data.errors);
+        for (const [key, value] of Object.entries(error.response.data.errors)) {
+          formError.push({
+            name: key,
+            message: value as string,
+          });
+        }
+        registerForm?.value?.setErrors(formError);
+      }
+      toast.add({
+        title: 'Error',
+        description: 'Something went wrong',
+        color: 'error',
+      });
+    });
 }
 </script>
