4fb85aecc2f0a2d4b5b2f83714f012b3aefc225e
iolite312 <68647023+iolite312@users.noreply.github.com>
2025-02-16 13:49:04 +0000

    More fields for street, etc..

--- vue/pages/profile.vue
+++ vue/pages/profile.vue
@@ -1,28 +1,64 @@
 <template>
-  <UForm
-    ref="registerForm"
-    :state="state"
-    class="flex flex-col gap-4"
-    @submit="updateUser"
-  >
-    <UFormField label="First name" name="first_name">
-      <UInput v-model="state.first_name" class="w-full" />
-    </UFormField>
-    <UFormField label="Last name" name="last_name">
-      <UInput v-model="state.last_name" class="w-full" />
-    </UFormField>
-    <UFormField label="Email" name="email">
-      <UInput v-model="state.email" class="w-full" disabled />
-    </UFormField>
-    <UFormField label="Password" name="password">
-      <UInput v-model="state.password" class="w-full" type="password" />
-    </UFormField>
-    <UFormField label="Confirm Password" name="confirmPassword">
-      <UInput v-model="state.confirmPassword" class="w-full" type="password" />
-    </UFormField>
-
-    <UButton label="Update" type="submit" variant="solid" class="self-end" />
-  </UForm>
+  <div>
+    <h2 class="text-2xl font-bold mb-4">profile</h2>
+    <UForm
+      ref="registerForm"
+      :state="state"
+      class="flex flex-col gap-4"
+      @submit="updateUser"
+    >
+      <div class="flex flex-row gap-4">
+        <div class="flex flex-col gap-4">
+          <UFormField label="First name" name="first_name">
+            <UInput v-model="state.first_name" class="w-full" />
+          </UFormField>
+          <UFormField label="Last name" name="last_name">
+            <UInput v-model="state.last_name" class="w-full" />
+          </UFormField>
+          <UFormField label="Email" name="email">
+            <UInput v-model="state.email" class="w-full" disabled />
+          </UFormField>
+          <UFormField label="Password" name="password">
+            <UInput v-model="state.password" class="w-full" type="password" />
+          </UFormField>
+          <UFormField label="Confirm Password" name="confirmPassword">
+            <UInput
+              v-model="state.confirmPassword"
+              class="w-full"
+              type="password"
+            />
+          </UFormField>
+        </div>
+        <div class="flex flex-col gap-4">
+          <UFormField label="Street" name="street">
+            <UInput
+              v-model="state.street"
+              class="w-full"
+              type="text"
+              placeholder="Bijv Straatnaam 1"
+            />
+          </UFormField>
+          <UFormField label="City" name="city">
+            <UInput
+              v-model="state.city"
+              class="w-full"
+              type="text"
+              placeholder="Bijv Amsterdam"
+            />
+          </UFormField>
+          <UFormField label="Postal code" name="postalcode">
+            <UInput
+              v-model="state.postalcode"
+              class="w-full"
+              type="text"
+              placeholder="1234AB"
+            />
+          </UFormField>
+        </div>
+      </div>
+      <UButton label="Update" type="submit" variant="solid" class="self-end" />
+    </UForm>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -48,6 +84,9 @@ const state = ref({
   password: '',
   confirmPassword: '',
   profile_picture: account.profile_picture,
+  street: account.street,
+  city: account.city,
+  postalcode: account.postalcode,
 });
 
 async function updateUser() {
