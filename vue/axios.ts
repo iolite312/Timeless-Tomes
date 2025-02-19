9387fa89c32a2f8dc536633f150505a3d342a0f3
iolite312 <68647023+iolite312@users.noreply.github.com>
2025-02-19 13:21:23 +0000

    Intercepting the axios error throw so that i can handle it my self instead of nuxt

--- vue/axios.ts
+++ vue/axios.ts
@@ -3,5 +3,17 @@ import axios from "axios";
 const axiosClient = axios.create({
   baseURL: useRuntimeConfig().public.BASE_URL + '/api',
 });
-// axiosClient.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+
+axiosClient.interceptors.response.use(
+  response => response,
+  error => {
+    console.error('API error:', error.response?.data || error.message);
+    throw createError({
+      statusCode: error.response?.status || 500,
+      statusMessage: error.response?.data?.message || 'Something went wrong',
+      fatal: true,
+    });
+  }
+);
+
 export default axiosClient;
